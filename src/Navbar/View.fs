fcd4f69f64edb88ad0be2f11a805457fdf03b44a
ThisFunctionalTom <tomas.leko@gmail.com>
2020-05-21 14:21:59 +0200

    add navbar brand icon

--- src/Navbar/View.fs
+++ src/Navbar/View.fs
@@ -9,15 +9,19 @@ open Fable.FontAwesome
 open Zanaptak.TypedCssClasses
 
 type Fa = CssClasses<"../node_modules/@fortawesome/fontawesome-free/css/fontawesome.min.css", Naming.PascalCase>
-//type Bulma = CssClasses<"../node_modules/bulma/css/bulma.min.css", Naming.PascalCase>
 
 let navBrand =
     Bulma.navbarBrand.div [
         Bulma.navbarItem.a [
             prop.href "#home"
             size.isSize4
-        //prop.classes [ Bulma.NavbarItem; Bulma.Title; Bulma.Is4 ]
-            prop.text "Fable Games" ] ]
+            prop.children [
+                Bulma.icon [
+                    prop.classes [ Fa.Fa; Fa.FaGamepad]
+                    prop.style [ style.width (length.em 2) ]
+                ]
+                Html.text "Fable Games" ] ]
+            ]
 
 let root navbarMenu =
     Bulma.navbar [
